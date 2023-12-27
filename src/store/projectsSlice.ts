a0f0098a962a037408f417e3e4a3d468e5d45feb
jyotibaskota <baskotaj7@gmail.com>
2023-12-27 11:11:38 -0500

    new files and folders created

---
+++ src/store/projectsSlice.ts
@@ -0,0 +1,34 @@
+// projectsSlice.ts
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  // Add more fields as needed
+}
+
+interface ProjectsState {
+  projects: Project[];
+}
+
+const initialState: ProjectsState = {
+  projects: [
+    // Add your projects here
+  ],
+};
+
+export const projectsSlice = createSlice({
+  name: "projects",
+  initialState,
+  reducers: {
+    addProject: (state, action: PayloadAction<Project>) => {
+      state.projects.push(action.payload);
+    },
+  },
+});
+
+export const { addProject } = projectsSlice.actions;
+
+export default projectsSlice.reducer;
